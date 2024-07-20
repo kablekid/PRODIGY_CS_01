33093987e0a93b3ffeb8e1eb118172a19de976a9
Kidus Berihun <67029622+masterkable@users.noreply.github.com>
2024-07-20 09:54:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,38 @@ import sys
 
 
 argumentList = sys.argv[1:] # remove the first argument which is the name of the program
-Options = "edm:"
-LongOptions = ["encrypt","decrypt","message"]
+Options = "edms:"
+LongOptions = ["encrypt","decrypt","message","step"]
 
-def ceaserCipher(process,message):
-   pass
+def ceaserCipher(process="",message="",step=0):
+   cipher = ""
+   for i in message:
+      if process == "encrypt":
+         number = ord(i) + step
+         cipher += chr(number)
+      elif process == "decrypt":
+         number = ord(i) - step
+         cipher += chr(number)
+   return cipher   
+
+
 
 try:
    arguments, values = getopt.getopt(argumentList,Options,LongOptions)
 
-
+   process,message,step = "","",""
    for CurrentArguemt,CurrentValue in arguments:
-      
+      print(CurrentArguemt+ "  " + CurrentValue)
       if CurrentArguemt in ("-e","--encrypt"):
-         ceaserCipher(CurrentArguemt,)
+         process = "encrypt"
       elif CurrentArguemt in ("-d","--decrypt"):
-         pass
+         process = "decrypt"
+      elif CurrentArguemt in ("-m","--message"):
+         message = CurrentValue
+      elif CurrentArguemt in ("-s","--step"):
+         step = CurrentValue
+   
+ 
 
 except getopt.error as error:
    print("Error has occuerd check your arguments")
