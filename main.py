3e3a10a8cfccf7f2a99e9e2d27d60c99d3cd43ef
kidus <kidusbk@gmail.com>
2024-07-23 21:54:46 +0300

    fixed the parsing problem and some touch ups on the output

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
 import getopt
 import sys
 
+def caesar_cipher(text, shift, mode):
+    shift = shift if mode == "encrypt" else -shift 
+    cipher_text = ""
+    for char in text:
+        if char.isalpha():
+            offset = ord('A') if char.isupper() else ord('a')
+            shifted_char = chr((ord(char) - offset + shift) % 26 + offset)
+            cipher_text += shifted_char
+        else:
+            cipher_text += char
+    return cipher_text
 
-argumentList = sys.argv[1:] # remove the first argument which is the name of the program
-Options = "edms:"
-LongOptions = ["encrypt","decrypt","message","step"]
+def main():
+    try:
+        
+        short_options = "edm:s:"
+        long_options = ["encrypt", "decrypt", "message=", "step="]
+        user_input = sys.argv[1:] # without the file name
+        opts, args = getopt.getopt(user_input, short_options, long_options)
+        
 
-def ceaserCipher(process="",message="",step=0):
-   cipher = ""
-   for i in message:
-      if process == "encrypt":
-         number = ord(i) + step
-         cipher += chr(number)
-      elif process == "decrypt":
-         number = ord(i) - step
-         cipher += chr(number)
-   return cipher   
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
 
-
-
-try:
-   arguments, values = getopt.getopt(argumentList,Options,LongOptions)
-
-   process,message,step = "","",""
-   for CurrentArguemt,CurrentValue in arguments:
-      print(CurrentArguemt+ "  " + CurrentValue)
-      if CurrentArguemt in ("-e","--encrypt"):
-         process = "encrypt"
-      elif CurrentArguemt in ("-d","--decrypt"):
-         process = "decrypt"
-      elif CurrentArguemt in ("-m","--message"):
-         message = CurrentValue
-      elif CurrentArguemt in ("-s","--step"):
-         step = CurrentValue
-   
- 
-
-except getopt.error as error:
-   print("Error has occuerd check your arguments")
+    
+    mode,message, step = "","", 0
+    for opt, arg in opts:
+        if opt in ("-e", "--encrypt"):
+            mode = "encrypt"
+        elif opt in ("-d", "--decrypt"):
+            mode = "decrypt"
+        elif opt in ("-m", "--message"):
+            message = arg
+        elif opt in ("-s", "--step"):
+            try:
+                step = int(arg)
+            except ValueError:
+                print("Error: Step value must be an integer")
+                sys.exit(2)
 
 
+    if not mode or not message or not step:
+        print("Usage: python script.py -[e|d] -m <message> -s <step>")
+        sys.exit(2)
+
+    print("message = {} \nstep = {} \nmode = {}".format(message,step,mode))
+    Ciphertext = caesar_cipher(message, step, mode)
+    if mode == "encrypt":
+        print("Ciphertext = " + Ciphertext )
+    elif mode == "decrypt":
+        print("Decrypted text = " + Ciphertext )
+        
+
+if __name__ == "__main__":
+    main()
